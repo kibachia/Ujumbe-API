26bb269c2f8a77aec0f57859f52193c07ef3ba69
Wainaina Kibachia <pwainainak@gmail.com>
2018-11-03 21:47:19 +0300

    Add files via upload

---
+++ application/config/email.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+//$config['mailpath'] = '/usr/sbin/sendmail';
+//$config['charset'] = 'iso-8859-1';
+//$config['wordwrap'] = TRUE;
+//$config['wrapchars'] = 76;
+//$config['useragent'] = 'CodeIgniter';
+//$config['protocol'] = 'smtp';
+//$config['smtp_host'] = '';
+//$config['smtp_user'] = '';
+//$config['smtp_pass'] = '';
+//$config['smtp_port'] = 25;
+//$config['smtp_timeout'] = 5;
+//$config['validate'] = false;
+//$config['priority'] = 3';
+//$config['crlf'] = '\n';
+//$config['newline'] = '\n';
+$config['mailtype'] = 'html';
